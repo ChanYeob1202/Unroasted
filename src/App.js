4a25c8f69f7e61a4bef50d7d0106e572b0ddd5a4
Michael Kim <michael.yeob@gmail.com>
2025-04-01 20:51:53 -0700

    Remove Education feature from main

--- src/App.js
+++ src/App.js
@@ -10,18 +10,7 @@ import Specialty from "./pages/blog_posts/Specialty";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Blog from "./pages/Blog";
-import Cart from "./pages/Cart";
 import Story from "./pages/Story";
-import Education from "./pages/Education";
-import Fundamentals from "./pages/education_page/fundamental/Fundamentals";
-import OriginIntro from "./pages/education_page/fundamental/originVarieties/OriginIntro";
-import CoffeeCherryToBean from "./pages/education_page/fundamental/originVarieties/CoffeeCherryToBean";
-import CoffeePlanBiologyCultivation from "./pages/education_page/fundamental/originVarieties/CoffeePlanBiologyCultivation";
-import TheImpactOfAltitudeAndClimate from "./pages/education_page/fundamental/originVarieties/TheImpactOfAltitudeAndClimate";
-import IntroClassification from "./pages/education_page/fundamental/bean_classification/Intro_classification";
-import ArabicaVsRobusta from "./pages/education_page/fundamental/bean_classification/Arabica-vs-Robusta";
-import Varieties from "./pages/education_page/fundamental/bean_classification/Varieties";
-import Grades from "./pages/education_page/fundamental/bean_classification/Grades";
 
 // Auth Pages
 import SignInPage from './pages/Auth/SignInPage';
@@ -62,71 +51,10 @@ function App() {
             </PublicRoutes>
           } />
 
-          <Route path="education" element={
-            <ProtectedRoutes>
-              <Education />
-            </ProtectedRoutes>
-          } />
-
-          <Route path="education/:id" element={<Education />} />
-          <Route path="cart" element={
-            <ProtectedRoutes>
-              <Cart />
-            </ProtectedRoutes>
-          } />
-
-          <Route path="/education/coffee-fundamentals" element={
-            <ProtectedRoutes>
-              <Fundamentals />
-            </ProtectedRoutes>
-          } />
-          <Route path="/education/coffee-fundamentals/origins&varieties" element={
-            <ProtectedRoutes>
-              <OriginIntro />
-            </ProtectedRoutes>
-          } />
-          <Route path="/education/coffee-fundamentals/origins&varieties/coffee-plant-biology-cultivation" element={
-            <ProtectedRoutes>
-              <CoffeePlanBiologyCultivation />
-            </ProtectedRoutes>
-          } />
-          <Route path="/education/coffee-fundamentals/origins&varieties/coffee-cherry-to-bean" element={
-            <ProtectedRoutes>
-              <CoffeeCherryToBean />
-            </ProtectedRoutes>
-          } />
-          <Route path="/education/coffee-fundamentals/origins&varieties/the-impact-of-altitude-and-climate" element={
-            <ProtectedRoutes>
-              <TheImpactOfAltitudeAndClimate />
-            </ProtectedRoutes>
-          } />
-          <Route path="/education/coffee-fundamentals/bean-classification" element={
-            <ProtectedRoutes>
-              <IntroClassification />
-            </ProtectedRoutes>
-          } />
-          <Route path = "/education/coffee-fundamentals/bean-classification/arabica-vs-robusta" element={
-            <ProtectedRoutes>
-              <ArabicaVsRobusta />
-            </ProtectedRoutes>
-          } />
-          <Route path="/education/coffee-fundamentals/bean-classification/varietals" element={
-            <ProtectedRoutes>
-              <Varieties />
-            </ProtectedRoutes>
-          } />
-          <Route path="/education/coffee-fundamentals/bean-classification/grades" element={
-            <ProtectedRoutes>
-              <Grades />
-            </ProtectedRoutes>
-          } />
-
           {/* Blog Routes */}
           <Route path ="/blog/traditional-vs-modern-coffee" element={<CreamTop />} />
           <Route path ="/blog/what-makes-coffee-specialty" element={<Specialty />} />
-          {/* {BlogRoutes.map(({ path, element }) => (
-            <Route key={path} path={path} element={element} />
-          ))} */}
+      
         </Route>
       </Routes>
       <TestDatabase />  
