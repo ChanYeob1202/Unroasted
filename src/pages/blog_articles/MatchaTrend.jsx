a7a285b85de21b3be4f031bc725dc8c732ae859e
ChanYeob1202 <michael.yeob@gmail.com>
2025-09-08 15:19:17 -0700

    Blog: add CMS article components and markdown assets

---
+++ src/pages/blog_articles/MatchaTrend.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react'
+import { motion } from 'framer-motion';
+import { marked } from 'marked';
+import ArticleFormat from '../../components/cms/ArticleFormat';
+
+export default function MatchaTrend() {
+  console.log("Matcha article mounted")
+  
+  return (
+      <ArticleFormat id = "v0e8lhlc3estujnhlve7y4bm"/>
+  )
+}
